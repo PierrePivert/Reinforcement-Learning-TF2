40a0e6afd8e916379d6285b7c42484504f4efb0d
PierrePivert <41760701+PierrePivert@users.noreply.github.com>
2020-01-14 16:07:25 +0100

    Add files via upload

--- Reinforcement Learning/Memory.py
+++ Reinforcement Learning/Memory.py
@@ -3,6 +3,14 @@ import numpy as np
 
 class UniformExperienceReplay(object):
     def __init__(self, memory_size, input_dim):
+        """
+        Implement a ring buffer (FIFO)
+
+        :param memory_size: size of the memory buffer
+        :param input_dim: shape of a single observation
+
+        NOTE : since all observation are stored in a list, we don't have to save consecutive frame as it is. We reconstruct them when calling get_batch
+        """
         self.cpt = 0
         self.input_dim = input_dim
         self.memory_size = memory_size
@@ -23,7 +31,13 @@ class UniformExperienceReplay(object):
             self.cpt += 1
 
     def get_batch(self, batch_size, deterministic_idx=None):
-        """ ... """
+        """
+        Get a batch of sample [obs, action, reward, obs_prime, done]
+        :param batch_size:
+        :param deterministic_idx:
+        :return:
+        NOTE : obs are reconstructed  from input_dim (stacking consecutive frame as proposed by DeepMind)
+        """
         """ Pull random samples """
         sample_idx = np.random.randint(low=0, high=self.cpt, size=batch_size) if deterministic_idx is None else deterministic_idx
         sample_mask = np.empty_like(sample_idx, dtype=bool)
